a1a68416ff6c4ec53797f394da211bd7755d1824
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2025-04-28 18:43:55 -0300

    feat: Change validateUser endpoint from GET to POST

--- paggo-backend/src/modules/auth/auth.controller.ts
+++ paggo-backend/src/modules/auth/auth.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Query, HttpException, HttpStatus, Get } from '@nestjs/common';
+import { Controller, Query, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get('validate')
+  @Post('validate')
   async validateUser(
     @Query('email') email: string,
     @Query('passwordHash') passwordHash: string,
