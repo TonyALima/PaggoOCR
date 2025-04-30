6e1f0b2435bbe7fe8a47ac1b18d9b934bbaae27b
Tony Albert Lima <80111558+TonyALima@users.noreply.github.com>
2025-04-30 17:01:23 -0300

    feat(auth): implement user registration and password hashing with Argon2 in backend

--- paggo-backend/src/modules/auth/auth.controller.ts
+++ paggo-backend/src/modules/auth/auth.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Body, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ValidateUserDto } from './dto/validate-user.dto'; // ajuste o caminho conforme necessário
+import { ValidateUserDto } from './dto/validate-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { ApiTags, ApiBody, ApiConsumes } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('validate')
+  @ApiConsumes('application/json')
+  @ApiBody({type: ValidateUserDto})
   async validateUser(
     @Body() validateUserDto: ValidateUserDto
-  ): Promise<{ message: string }> {
+  ) {
     const { email, passwordHash } = validateUserDto;
     const result = await this.authService.validateUser(email, passwordHash);
 
@@ -18,5 +23,33 @@ export class AuthController {
     }
 
     return { message: result };
+  }
+
+  @Post('signup')
+  @ApiConsumes('application/json')
+  @ApiBody({type: RegisterUserDto})
+  async registerUser(
+    @Body() registerUserDto: RegisterUserDto
+  ) {
+    const { name , email, password } = registerUserDto;
+
+    const validDomain = process.env.VALID_EMAIL_DOMAIN;
+    if (!validDomain) {
+      throw new HttpException("Valid email domain not set.", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    if (!email.endsWith(`@${validDomain}`)) {
+      throw new HttpException("Email invalid.", HttpStatus.BAD_REQUEST);
+    }
+
+    const userId = await this.authService.registerUser(name, email, password);
+
+    console.log("ok");
+
+    if (!userId) {
+      throw new HttpException('Registration db failed', HttpStatus.BAD_REQUEST);
+    }
+    console.log(`User ${userId} registered successfully.`);
+    const message = `User ${name} registered successfully.`;
+    return { message };
   }
 }
--- paggo-backend/src/modules/auth/auth.service.ts
+++ paggo-backend/src/modules/auth/auth.service.ts
@@ -1,18 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
+import * as argon2 from "argon2";
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  async validateUser(email: string, passwordHash: string): Promise<string> {
+  async validateUser(email: string, password: string): Promise<string> {
     const user = await this.prisma.user.findUnique({ where: { email } });
     if (!user) {
-        return "Invalid credentials";
+      return "Invalid credentials";
     }
-    if (passwordHash === user.passwordHash) {
-        return user.id;
+    const isPasswordValid = await this.verifyPassword(user.passwordHash, password);
+    if (isPasswordValid) {
+      return user.id;
     }
     return "Invalid credentials";
+  }
+
+  async registerUser(name: string, email: string, password: string): Promise<string> {
+    const hashedPassword = await this.hashPassword(password);
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hashedPassword,
+      },
+    });
+    return user.id;
+  }
+
+  /**
+   * Gera o hash de uma senha usando Argon2id.
+   * @param password A senha.
+   * @returns O hash da senha.
+   */
+  async hashPassword(password: string): Promise<string> {
+    return await argon2.hash(password, {
+      type: argon2.argon2id,
+    });
+  }
+
+  /**
+   * Verifica se a senha corresponde ao hash.
+   * @param hash O hash armazenado no banco de dados.
+   * @param password A senha fornecida pelo usuário.
+   * @returns true se a senha for válida.
+   */
+  async verifyPassword(hash: string, password: string): Promise<boolean> {
+    try {
+      return await argon2.verify(hash, password);
+    } catch {
+      return false;
+    }
   }
 }
